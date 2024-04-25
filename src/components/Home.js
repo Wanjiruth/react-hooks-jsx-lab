6fe6013a1c1816043b14e4bf28238ff920d674ba
Wanjiruth <njoki.ruth.wanjiku@gmail.com>
2024-04-25 23:48:52 +0300

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,14 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
-}
+  return (
+  <div id="home">
+    expect(h1).toHaveStyle({ color: "firebrick" });
+<h1 style={{ color: "firebrick" }}>Your Name is a Web Developer from {city}</h1>
 
+  </div>
+  );
+}
 export default Home;
+
+
